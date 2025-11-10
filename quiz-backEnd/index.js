aa550d0accaec333ac6b5a1de38542dd3f57e520
bielfigs <148137388+bielfigs@users.noreply.github.com>
2025-11-10 13:00:34 -0300

    Refactor app initialization in index.js
    Removed duplicate express and cors initialization.

--- quiz-backEnd/index.js
+++ quiz-backEnd/index.js
@@ -2,15 +2,13 @@ import express from "express";
 import cors from "cors";
 import quizRoutes from "./routes/quizRoutes.js";
 
-const cors = require("cors");
+
+const app = express();
+app.use(express.json());
 app.use(cors({
   origin: ["https://learnplay.vercel.app"], 
   methods: ["GET", "POST", "PUT", "DELETE"],
 }));
-
-const app = express();
-app.use(express.json());
-
 app.use("/quiz", quizRoutes);
 
 const PORT = process.env.PORT || 3001; 
