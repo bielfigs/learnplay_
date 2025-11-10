535eade25920742ed835724b6f841692f119b487
bielfigs <148137388+bielfigs@users.noreply.github.com>
2025-11-10 12:50:26 -0300

    Set up CORS for specific origin and methods
    Configured CORS to allow requests from a specific origin.

--- quiz-backEnd/index.js
+++ quiz-backEnd/index.js
@@ -2,6 +2,13 @@ import express from "express";
 import cors from "cors";
 import quizRoutes from "./routes/quizRoutes.js";
 
+const cors = require("cors");
+
+app.use(cors({
+  origin: ["https://learnplay.vercel.app"], 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
+
 const app = express();
 app.use(cors());
 app.use(express.json());
